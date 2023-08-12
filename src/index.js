56834bef8d74d1b3c26177d4c02bca1a76ba5421
Nikita Kolesnew <chattrain.kol7@gmail.com>
2023-08-12 17:09:43 +0300

    added settings for camera

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Canvas } from '@react-three/fiber';
 
-import './index.css';
 import { Experience } from './Experience';
 
+import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
 root.render(
-  <Canvas gl={{ antialias: false }}>
+  <Canvas shadows gl={{ antialias: false }} camera={{
+    fov: 45,
+    near: 0.1,
+    far: 200,
+    position: [-4, 3, 6]
+  }}
+  >
     <Experience />
   </Canvas>
 );
