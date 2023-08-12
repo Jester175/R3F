5c51c7f9cfb73e0c7f4612b3089de7560e8cb133
Nikita Kolesnew <chattrain.kol7@gmail.com>
2023-08-12 15:53:47 +0300

    initial app

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,12 +1,24 @@
+import { OrbitControls } from "@react-three/drei";
 import React from "react";
+
 
 export const Experience = () => {
   return (
     <>
+      <OrbitControls />
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
-      <mesh>
+      <mesh position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+      <mesh rotation-y={Math.PI * 0.25} position-x={2}>
         <boxGeometry />
-        <meshStandardMaterial color="yellowgreen" />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+      <mesh rotation-x={-Math.PI * 0.5} position-y={-1} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
       </mesh>
     </>
   );
