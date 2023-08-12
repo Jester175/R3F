8b68a84b486c48e6bd61160f6fa92e2706711a3a
Nikita Kolesnew <chattrain.kol7@gmail.com>
2023-08-12 17:10:01 +0300

    working with the shadow

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,22 +1,93 @@
-import { OrbitControls } from "@react-three/drei";
-import React from "react";
+import {
+  useHelper,
+  OrbitControls,
+  ContactShadows,
+  Sky,
+} from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import React, { useRef } from "react";
+import * as THREE from "three";
 
+// softShadows({
+//   frustum: 3.75,
+//   size: 0.005,
+//   near: 9.5,
+//   samples: 17,
+//   rings: 11,
+// });
 
 export const Experience = () => {
+  const dlRef = useRef();
+  const cubeRef = useRef();
+  useHelper(dlRef, THREE.DirectionalLightHelper, 1);
+
+  useFrame((state, delta) => {
+    const time = state.clock.elapsedTime;
+    cubeRef.current.position.x = 2 + Math.sin(time);
+
+    cubeRef.current.rotation.y += delta * 0.2;
+  }, []);
+
   return (
     <>
+      {/* <BakeShadows /> */}
+
+      <color args={["green"]} attach={"background"} />
+
       <OrbitControls />
-      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+
+      {/* <AccumulativeShadows
+        position={[0, -0.99, 0]}
+        scale={10}
+        color="#316d39"
+        opacity={0.8}
+        frames={Infinity}
+        temporal
+        blend={100}
+      >
+        <RandomizedLight
+          position={[1, 2, 3]}
+          amount={8}
+          radius={1}
+          ambient={0.5}
+          intensity={1}
+          bias={0.001}
+        />
+      </AccumulativeShadows> */}
+
+      {/* <ContactShadows
+        position={[0, -0.99, 0]}
+        scale={10}
+        resolution={128}
+        far={5}
+      /> */}
+
+      <directionalLight
+        ref={dlRef}
+        castShadow
+        position={[1, 2, 3]}
+        intensity={1.5}
+        shadow-mapSize={[1024, 1024]}
+        shadow-camera-near={1}
+        shadow-camera-far={10}
+        shadow-camera-top={5}
+        shadow-camera-right={5}
+        shadow-camera-bottom={-5}
+        shadow-camera-left={-5}
+      />
       <ambientLight intensity={0.5} />
-      <mesh position-x={-2}>
+
+      <Sky />
+
+      <mesh castShadow position-x={-2}>
         <sphereGeometry />
         <meshStandardMaterial color="orange" />
       </mesh>
-      <mesh rotation-y={Math.PI * 0.25} position-x={2}>
+      <mesh ref={cubeRef} castShadow rotation-y={Math.PI * 0.25} position-x={2}>
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
-      <mesh rotation-x={-Math.PI * 0.5} position-y={-1} scale={10}>
+      <mesh receiveShadow rotation-x={-Math.PI * 0.5} position-y={-1} scale={10}>
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
